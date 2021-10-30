2d7f21260ffbce441cec3bc02eb7d7eb6aad09ee
seventty <elmastigeron2015@gmail.com>
2021-10-29 21:15:29 -0400

    update

--- src/loaders/connection.js
+++ src/loaders/connection.js
@@ -2,9 +2,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config('./');
 
-const mongoPassword = process.env.MONGO_PASSWORD;
+const uri = process.env.MONGOATLAS_URI || process.env.MONGODB_URI;
 
-const uri = "mongodb+srv://linkilist:2PDAkInSOzLHzK8J@cluster0.sf0dn.mongodb.net/linkilist?retryWrites=true&w=majority";
 const db = mongoose.connection;
 
 mongoose.connect(uri).catch(err => console.log(err));
