7cec4641d81a0750153c67671ba8d4cdf8e7c416
seventty <elmastigeron2015@gmail.com>
2021-10-29 20:31:32 -0400

    Try catch in add command added

--- src/commands/crud/add.js
+++ src/commands/crud/add.js
@@ -2,14 +2,19 @@ import './../../loaders/connection.js';
 import LinkModel from './../../models/Links.js';
 import validUrl from 'valid-url';
 
-export default function add(bot, message, value){
+export default async function add(bot, message, value){
     if (validUrl.isUri(value[2])){
         const link = new LinkModel({
             name: value[1],
             url: value[2],
         });
-        link.save();
-        bot.sendMessage(message.chat.id, `Link agregado 📝\n\nNombre: ${value[1]}\nURL: ${value[2]}`);
+        try {
+            const savedRes = await link.save();
+            bot.sendMessage(message.chat.id, `Link agregado 📝\n\nNombre: ${value[1]}\nURL: ${value[2]}`)
+        } catch (error) {
+            bot.sendMessage(message.chat.id, `No pude guardar tu link ❌`)
+        }
+
     } 
     else {
         bot.sendMessage(message.chat.id, `Introduce una url existente`);
