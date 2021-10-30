834c20e7cfa54e180e80abb6109727b9f5b28f89
seventty <elmastigeron2015@gmail.com>
2021-10-29 20:11:16 -0400

    info command updated

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,3 +1,8 @@
+const add = "Este comando requiere de 2 parametros [nombre] [url] a pasar para ser desencadenado. Ejemplo: /add google https://google.com \nNota: La url ha de contener el protocolo https:// para poder ser ejecutado",
+read = "El comando read no requiere de ningun parametro, leera todos los links disponibles",
+update = "Este comando require de tres 3 parametros [id] [nombre] [url] para poder ser desencadenado. Ejemplo: /update 1 facebook https://facebook.com \nNota: El protocolo https:// tambien es obligatorio en la url",
+remove = "Este comando solo requiere de el id a borrar. Ejemplo: /remove 1";
+
 export default function info(bot, message){
-    bot.sendMessage(message.chat.id, "Los comandos para manipular links son: \n\n+add\n?read\n!update\n-remove");
+    bot.sendMessage(message.chat.id, `Los comandos para manipular links son: \n\n/add\n/read\n/update\n/remove\n\n${add}\n${read}\n${update}\n${remove}`);
 }
